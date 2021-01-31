cf852779424a4e43c842e3aa856119cc041edcb3
lkascueta <ascuetalk@gmail.com>
2021-01-31 21:08:40 +0800

    aa

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -289,7 +289,7 @@
         <footer class="sticky-footer bg-white">
             <div class="container my-auto">
                 <div class="copyright text-center my-auto">
-                    <span>Copyright &copy; Alejandro RH 2020</span>
+                    <span>Copyright &copy; Levie {{ now()->year}} </span>
                 </div>
             </div>
         </footer>
